99b0fe6290db8e38d13d820dd2eb594d5e78467c
whoadarshpandya <adarshpandya0@gmail.com>
2021-10-19 21:43:28 +0530

    sd

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
+const mongoose_1 = require("mongoose");
+const models_1 = require("./models");
 const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 const PORT = process.env.PORT;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+const db = (0, mongoose_1.connect)(process.env.DB_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+}, (err) => {
+    if (err) {
+        console.log("connection failed");
+    }
+    else {
+        console.log("ok");
+    }
+});
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const todo = new models_1.todoModel({
+        title: "dep",
+        description: "loyed",
+    });
+    yield todo.save();
+}))();
 app.get("/", (req, res) => {
     res.json({ msg: "hi fusion api" });
 });
--- src/index.ts
+++ src/index.ts
@@ -10,29 +10,29 @@ const PORT = process.env.PORT;
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
-// const db = connect(
-//   process.env.DB_URL!,
-//   {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: true,
-//   },
-//   (err) => {
-//     if (err) {
-//       console.log("connection failed");
-//     } else {
-//       console.log("ok");
-//     }
-//   }
-// );
-// (async () => {
-//   const todo: Document<Todo> = new todoModel({
-//     title: "a",
-//     description: "p",
-//   });
-//   await todo.save();
-// })();
+const db = connect(
+  process.env.DB_URL!,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  },
+  (err) => {
+    if (err) {
+      console.log("connection failed");
+    } else {
+      console.log("ok");
+    }
+  }
+);
+(async () => {
+  const todo: Document<Todo> = new todoModel({
+    title: "dep",
+    description: "loyed",
+  });
+  await todo.save();
+})();
 app.get("/", (req: Request, res: Response) => {
   res.json({ msg: "hi fusion api" });
 });
