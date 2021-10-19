321009dbf4a8475b42180710cbdfffe74023e603
whoadarshpandya <adarshpandya0@gmail.com>
2021-10-19 20:55:10 +0530

    fix:api rename

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const PORT = process.env.PORT || 2002;
 const app = (0, express_1.default)();
 app.get("/", (req, res) => {
-    res.json({ msg: "hello wrald" });
+    res.json({ msg: "hi fusion api" });
 });
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
