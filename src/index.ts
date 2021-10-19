ae7e1cc46d498e4f833fc4cf5eefee94a4c82416
whoadarshpandya <adarshpandya0@gmail.com>
2021-10-19 20:39:09 +0530

    initial

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+
+const PORT = 2002;
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ msg: "hello" });
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+});
